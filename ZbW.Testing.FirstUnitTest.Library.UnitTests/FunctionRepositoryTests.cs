6659d9a3952c813a241e2df7d4313a06ffca2506
Thierry <thierry@stadler.top>
2019-02-09 12:59:39 +0100

    test

---
+++ ZbW.Testing.FirstUnitTest.Library.UnitTests/FunctionRepositoryTests.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+
+namespace ZbW.Testing.FirstUnitTest.Library.UnitTests
+{
+    [TestFixture]
+
+    public class FunctionRepositoryTests
+    {
+        [Test]
+        public void TestNumber15()
+        {
+            var sut = new FunctionRepository();
+            var result = sut.GreaterThan10(15);
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void TestIsValidEMailadress()
+        {
+            var sut = new FunctionRepository();
+            var result = sut.IsValidEMailadress("sadf@das.com");
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void TestIsStringLongerThan10()
+        {
+            var sut = new FunctionRepository();
+            var result = sut.IsStringLongerThan10("zdhdddkdagod");
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void TestGetStringLenght()
+        {
+            var sut = new FunctionRepository();
+            var result = sut.GetStringLength("abcdefghiyklmnopqrstuvwxyz");
+            Assert.That(result, Is.EqualTo(26));
+        }
+
+        [Test]
+        public void TestAreNumbersInString()
+        {
+            var sut = new FunctionRepository();
+            var result = sut.AreNumbersInString("asdfadsfasdf0");
+            Assert.That(result, Is.True);
+        }
+
+        [Test, Category("short")]
+        public void IsIntValid()
+        {
+            var sut = new FunctionRepository();
+            var result = sut.IsIntValid("435435");
+            Assert.That(result, Is.True);
+        }
+    }
+}
---
+++ ZbW.Testing.FirstUnitTest.Library/FunctionRepository.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ZbW.Testing.FirstUnitTest.Library
+{
+    public class FunctionRepository
+    {
+        public bool GreaterThan10(int value)
+        {
+            var result = value > 10;
+            return result;
+        }
+
+        public bool IsValidEMailadress(string email)
+        {
+            var regex = new Regex(@"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$",
+                RegexOptions.Compiled);
+            return regex.IsMatch(email);
+        }
+
+        public bool IsStringLongerThan10(string value)
+        {
+            var result = value.Length > 10;
+            return result;
+        }
+
+        public int GetStringLength(string value)
+        {
+            var result = value.Length;
+            return result;
+        }
+
+        public bool AreNumbersInString(string value)
+        {
+            string strNummer = string.Empty;
+            foreach (char numChar in value.ToCharArray())
+            {
+                if (Char.IsNumber(numChar)) strNummer += numChar.ToString();
+            }
+
+            if (strNummer.Length == 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+//        public string ToUpperString(string value)
+//        {
+//
+//        }
+
+        public bool IsIntValid(string value)
+        {
+            int value1;
+            if (int.TryParse(value, out value1))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+   
+
+        }
+    }
+}
